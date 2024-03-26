8b91e8f6670db8c5cf7472c069a25d39699e2258
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-03-26 11:20:25 +0700

    add & use window size plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_task_archive/pages/home.dart';
+import 'package:window_size/window_size.dart';
 
-void main() {
+const _title = 'Flutter Task Archive';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+    final currentWindowFrame = (await getWindowInfo()).frame;
+    final currentScreenSize = (await getCurrentScreen())?.frame.size;
+
+    setWindowTitle(_title);
+
+    if (currentScreenSize != null && currentScreenSize.width > 2560) {
+      setWindowMinSize(const Size(600, 800));
+      setWindowFrame(
+        Rect.fromLTWH(
+            currentWindowFrame.left, currentWindowFrame.top, 800, 1000),
+      );
+    } else {
+      setWindowMinSize(const Size(400, 600));
+      setWindowFrame(
+        Rect.fromLTWH(
+            currentWindowFrame.left, currentWindowFrame.top, 600, 800),
+      );
+    }
+  }
+
   runApp(const MyApp());
 }
 
@@ -12,7 +40,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Task Archive',
+      title: _title,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
